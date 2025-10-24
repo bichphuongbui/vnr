970b8c38e8d405a043ad8930c9cce8a79b84b197
Phuong <bichphuongbui2015@gmail.com>
2025-10-25 00:42:55 +0700

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // hoặc base: '/vnr/' nếu deploy vào sub-path
 })
